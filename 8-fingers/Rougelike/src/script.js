2a6d66d0c58de8d368ebebcfdec991bcab073c54
Ted-bissada <tbissada@gmail.com>
2018-05-15 12:35:15 -0400

    Add files via upload

--- 8-fingers/Rougelike/src/script.js
+++ 8-fingers/Rougelike/src/script.js
@@ -27,10 +27,9 @@ let character = createCharacter();//creates and holds character
 let enemies = []; //array of all currently active enemies
 let miscObjects = []; // other interactive objects
 
-
 window.onload = function(){
-    let animation = requestAnimationFrame(gameLoop); //calls game loop 60 times a second
     drawBackground();
+    requestAnimationFrame(gameLoop); //calls game loop 60 times a second
 };
 
 
@@ -40,7 +39,7 @@ function gameLoop()
     render();
     userInputHandler();
     gameLogic();
-    animation = requestAnimationFrame(gameLoop);
+    requestAnimationFrame(gameLoop);
 }
 
 function render() //clears screen and draws all elements in turn
@@ -52,28 +51,29 @@ function render() //clears screen and draws all elements in turn
 
 function drawBackground()// draws background layer should only be called during screen transitions
 {
-    //for(let i =0;i<20;i++)
-        //for(let q =0;q<20;q++)
-    bgSurface.drawImage(backgroundImage,31,31,81,93,110,110,81*3,93*3);
+    bgSurface.drawImage(backgroundImage,31,31,81,93,110,110,81*3,93*3);//background image
 }
 
 function drawMain() //draws all enemies player and interactive objects
 {
-    fgSurface.clearRect(0,0,600,600);
-    fgSurface.drawImage(backgroundImage,195,160,15,20,character.cordinates[0],character.cordinates[1],15*3,20*3);
-    for (let i=0;i<character.bullets.length;i++)
-        fgSurface.drawImage(backgroundImage,190,130,10,10,character.bullets[i].cordinates[0],character.bullets[i].cordinates[1],
-            character.bullets[i].cordinates[2],character.bullets[i].cordinates[3]);
+    fgSurface.clearRect(0,0,600,600); //clear game area
+    fgSurface.drawImage(backgroundImage,195,160,15,20,character.cordinates[0],character.cordinates[1],15*3,20*3); //character
+    for (let i=0;i<character.bullets.length;i++) //draws bullets
+    {
+        let temp = character.bullets[i];
+        fgSurface.drawImage(backgroundImage, temp.sprite[0], temp.sprite[1], temp.sprite[2], temp.sprite[3], temp.cordinates[0], temp.cordinates[1],
+            temp.sprite[2], temp.sprite[3]);
+    }
 
 }
 
-function drawUI() // draws UI ontop of everything else
+function drawUI() // draws UI ontop of everything else currently showing debug info
 {
     uiSurface.clearRect(0,600,600,700);
     uiSurface.font = "10px Courier New";
     uiSurface.fillText("x: "+character.cordinates[0].toString(), 50, 610);
     uiSurface.fillText("y: "+character.cordinates[1].toString(), 50, 630);
-    uiSurface.fillText("state: "+character.state.toString(), 100, 610);
+    uiSurface.fillText("vector: "+character.cordinates[2].toString() +character.cordinates[3].toString(), 100, 610);
     uiSurface.fillText("attack cooldown: "+character.attackChargeTimer.toString(), 100, 630);
 }
 
@@ -81,30 +81,38 @@ function drawUI() // draws UI ontop of everything else
 function userInputHandler() //accepts and applies player input
 {
     if(keysPressed.includes(37))//left
-        character.moveLeft();
+        character.cordinates[2]=-1;
     if(keysPressed.includes(39))//right
-        character.moveRight();
+        character.cordinates[2]=1;
     if(keysPressed.includes(38))//up
-        character.moveUp();
+        character.cordinates[3]=-1;
     if(keysPressed.includes(40))//down
-        character.moveDown();
+        character.cordinates[3]=1;
     if(keysPressed.includes(32))//spacebar
         character.attack();
 }
 
 function gameLogic() //updates all game functions and ai
 {
-    for (let i=0;i<character.bullets.length;i++)
+    for (let i=0;i<character.bullets.length;i++) //itererates through bullet list
     {
-        character.bullets[i].move();
-        if (character.bullets[i].timeToLive() <= 0)
+        character.bullets[i].move(); // moves bullets
+        if (character.bullets[i].timeToLive() <= 0) //changes and returns time to live
         {
-            character.bullets.splice(i,1);
-            i--;
+            character.bullets.splice(i,1); //destroy expires bullets
+            i--; //moves back one plaec in the list
         }
     }
-    if (character.attackChargeTimer > 0)
+    if (character.attackChargeTimer > 0)//recharging attack timer
         character.attackChargeTimer--;
+    character.moveFinal(); //uses character vector to move it
+    if(character.cordinates[2] != 0 || character.cordinates[3]!= 0) //applies character movment to bullet firing direction unless char is not moving
+    {
+        character.bulletDirection[0] = character.cordinates[2];
+        character.bulletDirection[1] = character.cordinates[3];
+    }
+    character.cordinates[2] = 0; //resets character movment vector
+    character.cordinates[3] = 0;
 }
 
 
@@ -116,7 +124,7 @@ function roughCollision(x1,y1,w1,h1,x2,y2,w2,h2) //takes the x,y,width and heigh
         h1 + y1 > y2)
 }
 
-function fineCollision()//determines the angle of collision so the walls can apply counterforce
+function fineCollision()//will use penetration testing to determine what vector to apply to character to move out of walls
 {
 
 }
@@ -160,34 +168,36 @@ function keyUpHandler(e) //removes specified key from array
 function createCharacter() //generates and contains game character
 {
     let obj = {};
-    obj.cordinates = [300,300,15,20]; //player characters cordinates stored as x,y pair
-    obj.health = 5;
-    obj.state = 1; //player characters current animation state
-    obj.currentWeapon = generateWeapon(1,20,1,5,0);
-    obj.attackChargeTimer = 0;
-    obj.floorLevel = 0;
-    obj.speed= 2;
-    obj.moveLeft = function(){character.cordinates[0]-=character.speed;character.state = 4;};
-    obj.moveRight = function(){character.cordinates[0]+=character.speed;character.state = 2;};
-    obj.moveUp = function(){character.cordinates[1]-=character.speed;character.state = 1;};
-    obj.moveDown = function(){character.cordinates[1]+=character.speed;character.state = 3;};
+    obj.cordinates = [300,300,0,0]; //player characters cordinates stored as x,y pair and player movement vector
+    obj.bulletDirection = [1,0];  //last direction facing used for
+    obj.bullets = []; //bullets in the air
+    obj.health = 5; // health
+    obj.state = 0; //player characters current animation state
+    obj.currentWeapon = generateWeapon(1,60,3,2,7,0);//dmg, bullet time to live,bullet speed, max bullets, reload timer, special attribute
+    obj.attackChargeTimer = 0; //keeps track of reload time for weapon
+    obj.speed= 2; //movement speed
     obj.attack = function()
     {
-        if (this.attackChargeTimer == 0)
+        if (this.attackChargeTimer == 0 && this.bullets.length < this.currentWeapon.bullets)
         {
-            character.bullets.push(newBullet());
+            this.bullets.push(newBullet());
             this.attackChargeTimer = this.currentWeapon.reload;
         }
     };
-    obj.bullets = [];
+    obj.moveFinal = function ()
+    {
+        this.cordinates[0] += Math.floor(this.cordinates[2] * this.speed);
+        this.cordinates[1] += Math.floor(this.cordinates[3] * this.speed);
+    };
     return (obj);
 }
 
-function generateWeapon(dmg,range,bullets,reload,special)
+function generateWeapon(dmg,range,speed,bullets,reload,special)
 {
     let obj = {};
     obj.dmg = dmg;
     obj.range = range;
+    obj.speed = speed;
     obj.bullets = bullets;
     obj.reload = reload;
     obj.special = special;
@@ -197,20 +207,16 @@ function generateWeapon(dmg,range,bullets,reload,special)
 function newBullet()
 {
     let obj = {};
-    obj.speed = 5;
-    obj.cordinates = [character.cordinates[0],character.cordinates[1],10,10];
-    obj.direction = character.state;
+    obj.speed = character.currentWeapon.speed;
+    obj.cordinates = [character.cordinates[0],character.cordinates[1],character.bulletDirection[0]*obj.speed,character.bulletDirection[1]*obj.speed];
+    obj.sprite = [190,130,10,10];
     obj.lifetime = character.currentWeapon.range;
     obj.timeToLive = function(){this.lifetime--;return(this.lifetime)};
-    obj.move = function () {
-        if(this.direction === 4)
-            this.cordinates[0]-=this.speed;
-        else if(this.direction === 2)
-            this.cordinates[0]+=this.speed;
-        else if(this.direction === 1)
-            this.cordinates[1]-=this.speed;
-        else if(this.direction === 3)
-            this.cordinates[1]+=this.speed;};
+    obj.move = function ()
+    {
+        this.cordinates[0] += this.cordinates[2];
+        this.cordinates[1] += this.cordinates[3];
+    };
     return obj;
 }
 
@@ -220,7 +226,7 @@ function enemyType1(x,y,level)
     obj.cordinates = [x,y,10,10];
     obj.level = 1;
     obj.health = level+2;
-    obj.damage = Math.floor(level/2+1);
+    obj.damage = Math.floor((level/2)+1);
     obj.ai = function ()//called once per frame to decide on what enemy should do
     {
 
@@ -233,8 +239,8 @@ function enemyType2(x,y,level)
     let obj ={};
     obj.cordinates = [x,y,10,10];
     obj.level = 1;
-    obj.health = level+2;
-    obj.damage = Math.floor(level/2+1);
+    obj.health = level*2;
+    obj.damage = Math.floor(level);
     obj.ai = function ()//called once per frame to decide on what enemy should do
     {
 
@@ -247,7 +253,7 @@ function enemyType3(x,y,level)
     let obj ={};
     obj.cordinates = [x,y,10,10];
     obj.health = level+2;
-    obj.damage = Math.floor(level/2+1);
+    obj.damage = Math.floor(2+(level*1.5));
     obj.ai = function ()//called once per frame to decide on what enemy should do
     {
 
